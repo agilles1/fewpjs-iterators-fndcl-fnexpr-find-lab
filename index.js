745ed8dd47a2f0b7bb319264313139669e834429
agilles1 <adam.g.tpt@gmail.com>
2021-02-20 22:48:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(a) {
+ let year = undefined
+     a.find(function(game) {
+      if (game.result === 'W') { 
+        console.log(game.year)
+        year = game.year
+        }
+    })
+    return year
+}
